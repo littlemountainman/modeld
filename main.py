faaa8c04154e32738b3a79a8ab74f2f51ea6fa46
MankaranSingh <mankaran32@gmail.com>
2020-05-13 22:30:31 +0530

    Fixes OOM, framereader errors.
    This PR does following changes:
    
     - Make lane lines and path thinner in plot
     - Load raw_images on the fly rather than loading all of them at once for less memory usage
     - Remove OPs frame reader dependency and use OpenCV for frame reading instead.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from common.transformations.camera import transform_img, eon_intrinsics
 from common.transformations.model import medmodel_intrinsics
 import numpy as np
 from tqdm import tqdm
-from tools.lib.framereader import FrameReader, MP4FrameReader, MKVFrameReader 
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -22,18 +21,16 @@ MAX_REL_V = 10.
 LEAD_X_SCALE = 10
 LEAD_Y_SCALE = 10
 
-fr = FrameReader(camerafile)
 cap = cv2.VideoCapture(camerafile)
 
 imgs = []
-raw_imgs = []
-for i in tqdm(range(1000)):
-  imgs.append(fr.get(i, pix_fmt='yuv420p')[0].reshape((874*3//2, 1164)))
-  ret, frame = cap.read()
-  raw_imgs.append(frame)
 
-raw_imgs = np.array(raw_imgs)
-print(raw_imgs.shape)
+for i in tqdm(range(1000)):
+  ret, frame = cap.read()
+  img_yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV_I420)
+  imgs.append(img_yuv.reshape((874*3//2, 1164)))
+ 
+
 def frames_to_tensor(frames):                                                                                               
   H = (frames.shape[1]*2)//3                                                                                                
   W = frames.shape[2]                                                                                                       
@@ -57,6 +54,8 @@ frame_tensors = frames_to_tensor(np.array(imgs_med_model)).astype(np.float32)/12
 state = np.zeros((1,512))
 desire = np.zeros((1,8))
 
+cap = cv2.VideoCapture(camerafile)
+
 for i in tqdm(range(len(frame_tensors) - 1)):
   inputs = [np.vstack(frame_tensors[i:i+2])[None], desire, state]
   outs = supercombo.predict(inputs)
@@ -64,28 +63,26 @@ for i in tqdm(range(len(frame_tensors) - 1)):
   # Important to refeed the state
   state = outs[-1]
   pose = outs[-2]
-
+  ret, frame = cap.read()
+  frame = cv2.resize(frame, (640, 420))
   # Show raw camera image
-  cv2.imshow("modeld", raw_imgs[i])
+  cv2.imshow("modeld", frame)
   # Clean plot for next frame
   plt.clf()
   plt.title("lanes and path")
   # lll = left lane line
-  plt.scatter(parsed["lll"], range(0,192), c="b")
+  plt.plot(parsed["lll"][0], range(0,192), "b-", linewidth=1)
   # rll = right lane line
-  plt.scatter(parsed["rll"], range(0, 192), c="r")
+  plt.plot(parsed["rll"][0], range(0, 192), "r-", linewidth=1)
   # path = path cool isn't it ?
-  plt.scatter(parsed["path"], range(0, 192), c="g")
-  print(np.array(pose[0,:3]).shape)
+  plt.plot(parsed["path"][0], range(0, 192), "g-", linewidth=1)
+  #print(np.array(pose[0,:3]).shape)
   #plt.scatter(pose[0,:3], range(3), c="y")
   
   # Needed to invert axis because standart left lane is positive and right lane is negative, so we flip the x axis
   plt.gca().invert_xaxis()
-  plt.pause(0.05)
+  plt.pause(0.001)
   if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
 plt.show()
-  
-
-
